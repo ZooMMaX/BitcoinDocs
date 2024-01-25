357be823992b70e19f7b2a4967920519233ce08a
ZooMMaX <zoommax2009@gmail.com>
2024-01-26 00:38:43 +0300

    add argument parser

--- src/main/java/ru/zoommax/bitcoindocs/parser/Blocks.java
+++ src/main/java/ru/zoommax/bitcoindocs/parser/Blocks.java
@@ -2,22 +2,26 @@ package ru.zoommax.bitcoindocs.parser;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 public class Blocks {
     private String description;
+    private Arguments arguments;
     private Result result;
     private List<String> examples;
 
     public Blocks(String raw) {
         String[] lines = raw.split("\n");
-        String[] parts = {"","",""};
+        String[] parts = {"","","",""};
         int resultIndex = 0;
+        int argumentsIndex = 0;
         int exampleIndex = 0;
         for (int i = 0; i < lines.length; i++) {
+            if (lines[i].startsWith("Arguments") && argumentsIndex == 0) {
+                argumentsIndex = i-1;
+            }
             if (lines[i].startsWith("Result") && resultIndex == 0) {
-                resultIndex = i-1;
+                resultIndex = i - 1;
             }
             if (lines[i].startsWith("Example")) {
                 exampleIndex = i;
@@ -25,25 +29,39 @@ public class Blocks {
         }
 
         for (int i = 0; i < lines.length; i++) {
-            if (i < resultIndex) {
+            if (i < argumentsIndex || argumentsIndex == 0 && i< resultIndex) {
                 parts[0] = parts[0] + lines[i]+ "\n";
             }
-            if (i > resultIndex && i < exampleIndex) {
+            if (i > argumentsIndex && i < resultIndex && argumentsIndex != 0) {
                 parts[1] = parts[1] + lines[i]+ "\n";
             }
-            if (i > exampleIndex) {
+            if (i > resultIndex && i < exampleIndex  && resultIndex != 0) {
                 parts[2] = parts[2] + lines[i]+ "\n";
+            } else if (i > resultIndex && exampleIndex == 0 && resultIndex != 0) {
+                parts[2] = parts[2] + lines[i]+ "\n";
+            }
+            if (i > exampleIndex  && exampleIndex != 0) {
+                parts[3] = parts[3] + lines[i]+ "\n";
             }
         }
 
+        if (resultIndex == 0 && argumentsIndex == 0 && exampleIndex == 0) {
+            parts[0] = raw;
+        }
+
         this.description = parts[0].trim();
-        this.result = new Result(parts[1]);
-        this.examples = new ArrayList<>(Arrays.asList(parts[2].split(">")));
+        this.arguments = new Arguments(parts[1]);
+        this.result = new Result(parts[2]);
+        this.examples = new ArrayList<>(Arrays.asList(parts[3].split(">")));
         this.examples.replaceAll(String::trim);
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public Arguments getArguments() {
+        return arguments;
     }
 
     public Result getResult() {
